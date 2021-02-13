4974b8d0d6d6081eac1ccd7f36d26c8cdd01e6d4
visibait <33099726+visibait@users.noreply.github.com>
2021-02-13 15:07:44 +0100

    Update heartbeat.lua

--- lealtad-heartbeat/heartbeat.lua
+++ lealtad-heartbeat/heartbeat.lua
@@ -21,5 +21,5 @@ end)
 
 -- SI EL SERVER DEJA DE APARECER EN LA LISTA, SE DEBE BAJAR EL ULTIMO WAIT TIME QUE ESTÁ PUESTO EN 20000.
 
--- Muy importante añadir esto en el top del Server.CFG para permitir el envío de comandos: 
+-- Muy importante añadir esto en la parte más alta (debajo de los endpoints) y en la parte de abajo del todo del Server.CFG (dos veces, si xD, por si acaso) para permitir el envío de comandos al scrpipt: 
 -- add_ace resource.lealtad-heartbeat command.heartbeat allow
