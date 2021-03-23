24abec6d8a5e89d5f8576f0bdfe74a8b047b7c3a
Bombay <72895600+XiosBombay@users.noreply.github.com>
2021-03-23 13:43:37 -0400

    Update heartbeat.lua

--- lealtad-heartbeat/heartbeat.lua
+++ lealtad-heartbeat/heartbeat.lua
@@ -1,19 +1,20 @@
 -- Server Listing Fix by VisiBait for LealtadRP 
+local time = 50
 
 Citizen.CreateThread(function()
     while true do
     ExecuteCommand("heartbeat") -- Heartbeat 1
-    Citizen.Wait(50)
+    Citizen.Wait(time)
     ExecuteCommand("heartbeat") -- Heartbeat 2
-    Citizen.Wait(50)
+    Citizen.Wait(time)
     ExecuteCommand("heartbeat") -- Heartbeat 3
-    Citizen.Wait(50)
+    Citizen.Wait(time)
     ExecuteCommand("heartbeat") -- Heartbeat 4
-    Citizen.Wait(50)
+    Citizen.Wait(time)
     ExecuteCommand("heartbeat") -- Heartbeat 5
-    Citizen.Wait(50)
+    Citizen.Wait(time)
     ExecuteCommand("heartbeat") -- Heartbeat 6
-    Citizen.Wait(50)
+    Citizen.Wait(time)
     ExecuteCommand("heartbeat") -- Heartbeat 7
     Citizen.Wait(20000)  -- Los heartbeats se envian cada 20seg para evitar que el server deje de aparecer en la lista
     end
